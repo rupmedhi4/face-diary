bedfb4b4902fff71f16e88bf03800177d5c6ce93
Rup medhi <rupmmedhi@gmail.com>
2025-06-26 23:43:18 +0530

    jwt

--- jwt/generateToken.js
+++ jwt/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 
-const createTokenAndSaveCookie = (user,faceId,res) => {
+const createTokenAndSaveCookie = (user, faceId, res) => {
     const token = jwt.sign(
         { id: user._id, faceId },
         process.env.JWT_TOKEN,
@@ -9,14 +9,15 @@ const createTokenAndSaveCookie = (user,faceId,res) => {
             expiresIn: '14d'
         })
 
-    res.cookie("jwt", token, {
-        httpOnly: false,
-        secure: false,
-        sameSite: "Lax",
-        maxAge: 14 * 24 * 60 * 60 * 1000
-    })
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: true, // ✅ true for https, false for dev/local
+        sameSite: 'None', // ✅ for cross-site cookie
+        maxAge: 3 * 24 * 60 * 60 * 1000 // 3 days
+    });
+ 
     console.log(token);
-    
+
 }
 
 export { createTokenAndSaveCookie }
