cd13ae52e2a86981296e967fc1d7da4def478d97
Rup medhi <rupmmedhi@gmail.com>
2025-06-27 15:04:32 +0530

    update

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {  signupOrLogin, verifyJwt } from "../controllers/user.controller.js";
+import {  signupOrLogin} from "../controllers/user.controller.js";
 import { isAuthMiddleware, isLogin } from "../middleware/authMiddleware.js";
 import { diaryEntries, getDiaryEntries } from "../controllers/diary.entries.model.js";
 
