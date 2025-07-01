d2ff7af9936dd4ee2f04463313976e58ad6bf145
Rup medhi <rupmmedhi@gmail.com>
2025-07-01 20:33:34 +0530

    update

--- jwt/generateToken.js
+++ jwt/generateToken.js
@@ -8,12 +8,12 @@ const createTokenAndSaveCookie = (user, res) => {
     { expiresIn: '14d' }
   );
 
- res.cookie("jwt", token, {
-  httpOnly: false,      
-  secure: true,        
-  sameSite: "None",     
-  maxAge: 14 * 24 * 60 * 60 * 1000
-});
+  res.cookie("jwt", token, {
+    httpOnly: false,
+    secure: true,
+    sameSite: "None",
+    maxAge: 14 * 24 * 60 * 60 * 1000
+  });
 
 
   return token
